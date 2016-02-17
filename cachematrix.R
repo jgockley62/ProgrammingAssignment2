9e23ec400fb7517dc2694f6f50a9d5cc03ddc3f2
jgockley <jgockley62@gmail.com>
2016-02-17 16:06:41 -0500

    my notes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Here's a test matrix to test the code:
+TEST<-rbind(c(3,0,2),c(2,0,-2),c(0,1,1))
+TEST2<-rbind(c(3,0,1),c(2,0,-2),c(0,1,1))
 
-## Write a short comment describing this function
+##makeCacheMatrix takes a matrix and returns a list of funtions
+#Example Call: foo<-makeCacheMatrix(TEST)
+#Result:
+#1-foo$get: stores a function foo$get() - which returns the input matrix when foo$get() is called 
+#2-foo$getsolve: stores a function foo$getsolve() - which returns the inverse of the input matrix
+#3-foo$set: returns a null variable
+#4-foo$setsolve: function to call solve on variable m (which is a null variable to be replaced later if necesary)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve is a function to examine a makeCacheMatrix list object (example foo from the above test code)
+#Searches the object from makeCacheMatrix for the stored inverse matrix. If it exists it is returned.
+#If it isn't stored it is calulated and returned
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
